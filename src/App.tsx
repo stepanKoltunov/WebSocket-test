3b75025f91d219d3f150ae9d87bbcc8223b7435a
koltunov <koltunov@protei.ru>
2025-08-09 01:38:23 +0300

    add websocket app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import {useEffect, useRef, useState} from "react";
+
 function App() {
+    const [btnValue, setBtnValue] = useState<boolean>(false)
+    const [price, setPrice] = useState<number | null>(null);
+    const socketData = useRef<WebSocket | null>(null)
+
+    useEffect(() => {
+        if (btnValue) {
+            socketData.current = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade')
+            socketData.current.onmessage = (event) => {
+                const data = JSON.parse(event.data);
+                const newPrice = parseFloat(data.p);
+                setPrice(newPrice);
+            };
+            return () => {
+                if (socketData.current) {
+                    socketData.current.close();
+                }
+            };
+        }
+    },[btnValue])
 
     return (
       <div>
-          123
+          <span>{price}</span>
+          <button onClick={() => setBtnValue(prev => !prev)}>Старт/Стоп</button>
       </div>
   )
 }
